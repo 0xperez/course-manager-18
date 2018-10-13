f121582149d8798e38f18bf6449afa9d89cfc6cb
the-radiator <34714232+davide-perez@users.noreply.github.com>
2018-10-13 19:33:28 +0200

    txt

--- CourseManager18/src/application/Main.java
+++ CourseManager18/src/application/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.BorderPane;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		System.out.print("In start\n");
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
