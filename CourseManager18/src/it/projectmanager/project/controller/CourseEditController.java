ff6b36c7e0395e9ebf9b1f0ffdb40a99e72ec16a
the-radiator <34714232+davide-perez@users.noreply.github.com>
2018-10-16 00:39:47 +0200

    Edit panel now working

--- CourseManager18/src/it/projectmanager/project/controller/CourseEditController.java
+++ CourseManager18/src/it/projectmanager/project/controller/CourseEditController.java
@@ -2,6 +2,7 @@ package it.projectmanager.project.controller;
 
 import it.projectmanager.project.MainClass;
 import it.projectmanager.project.model.Course;
+import it.projectmanager.project.utils.DialogUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
@@ -31,6 +32,7 @@ public class CourseEditController {
 	@FXML
 	private TextField project;
 	
+	private Course course;
 	
 	private MainClass main;
 	
@@ -42,6 +44,8 @@ public class CourseEditController {
 	
 	public void setCourse(Course c) {
 		
+		this.course = c;
+		
 		name.setText(c.getName());
 		year.setText(c.getYear()+ "");
 		professor.setText(c.getProfessor());
@@ -51,6 +55,24 @@ public class CourseEditController {
 		folder.setText(c.getFolder());
 		project.setText(c.getProject());
 		
+	}
+	
+	
+	//input has to be validated prior be edited
+	@FXML
+	public void editCourse() {
+		course.setName(name.getText());
+		course.setYear(Integer.parseInt(year.getText()));
+		course.setProfessor(professor.getText());
+		course.setMark(Integer.parseInt(mark.getText()));
+		course.setCredits(Integer.parseInt(mark.getText()));
+		course.setField(field.getText());
+		course.setFolder(folder.getText());
+		course.setProject(project.getText());
+		
+		DialogUtils.showInfo("Course Manager 2018", "Course information edited succesfully!");
+		
+		main.showCoursePane();
 	}
 
 }
