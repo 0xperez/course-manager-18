a5c13cbec779b3e273b937a0a91fbe7a822480d0
the-radiator <34714232+davide-perez@users.noreply.github.com>
2018-10-16 20:40:53 +0200

    Defining some tests

--- CourseManager18/src/it/projectmanager/project/utils/CourseEditValidator.java
+++ CourseManager18/src/it/projectmanager/project/utils/CourseEditValidator.java
@@ -1,11 +1,15 @@
 package it.projectmanager.project.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import it.projectmanager.project.controller.CourseEditController;
 
 //https://stackoverflow.com/questions/24702542/how-to-change-the-color-of-text-in-javafx-textfield
 public class CourseEditValidator {
 	
 	private CourseEditController controller;
+	
 	
 	public CourseEditValidator(CourseEditController controller) {
 		this.controller = controller;
@@ -15,5 +19,11 @@ public class CourseEditValidator {
 	public boolean isValid() {
 		return true;
 	}
+	
+	
+	public boolean checkName() {
+		return false;
+	}
+	
 
 }
